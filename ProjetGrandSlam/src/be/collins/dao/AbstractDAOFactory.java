35f92aef9bb75efc4004b96c90daad506e69d5fe
Agedo collins <agedocollins@gmail.com>
2020-08-06 01:47:15 +0200

    Implémentation de la partie connexion à la base de donnée

---
+++ ProjetGrandSlam/src/be/collins/dao/AbstractDAOFactory.java
@@ -0,0 +1,28 @@
+package be.collins.dao;
+import be.collins.pojo.*;
+
+public abstract class AbstractDAOFactory {
+	
+	public static  final int DAO_FACTORY = 0;
+		
+	public abstract DAO<Personne> getPersonneDAO();
+	public abstract DAO<Equipe> getEquipeDAO();
+	public abstract DAO<Match> getMatchDAO();
+	public abstract DAO<Joueur> getJoueurDAO();
+	public abstract DAO<Arbitre> getArbitreDAO();
+	public abstract DAO<Admin> getAdminDAO();
+	public abstract DAO<Court> getCourtDAO();
+	public abstract DAO<Ordonnancement> getOrdonnancementDAO();
+	public abstract DAO<Tournoi> getTournoiDAO();
+	
+	public static AbstractDAOFactory getFactory(int type)
+	{
+		switch(type)
+		{
+			case DAO_FACTORY:
+				return new DAOFactory();
+			default:
+				return null;
+		}
+	}
+}
---
+++ ProjetGrandSlam/src/be/collins/dao/DAOFactory.java
@@ -0,0 +1,64 @@
+package be.collins.dao;
+
+import java.sql.Connection;
+import be.collins.pojo.*;
+
+public class DAOFactory extends AbstractDAOFactory {
+	
+	protected static final Connection conn = ConnexionDB.getInstance();
+
+	@Override
+	public DAO<Personne> getPersonneDAO() {
+		// TODO Auto-generated method stub
+		return new PersonneDAO(conn);
+	}
+
+	@Override
+	public DAO<Equipe> getEquipeDAO() {
+		// TODO Auto-generated method stub
+		return new EquipeDAO(conn);
+	}
+
+	@Override
+	public DAO<Match> getMatchDAO() {
+		// TODO Auto-generated method stub
+		return new MatchDAO(conn);
+	}
+
+	@Override
+	public DAO<Joueur> getJoueurDAO() {
+		// TODO Auto-generated method stub
+		return new JoueurDAO(conn);
+	}
+
+	@Override
+	public DAO<Arbitre> getArbitreDAO() {
+		// TODO Auto-generated method stub
+		return new ArbitreDAO(conn);
+	}
+
+	@Override
+	public DAO<Court> getCourtDAO() {
+		// TODO Auto-generated method stub
+		return new CourtDAO(conn);
+	}
+
+	@Override
+	public DAO<Ordonnancement> getOrdonnancementDAO() {
+		// TODO Auto-generated method stub
+		return new OrdonnancementDAO(conn);
+	}
+
+	@Override
+	public DAO<Tournoi> getTournoiDAO() {
+		// TODO Auto-generated method stub
+		return new TournoiDAO(conn);
+	}
+
+	@Override
+	public DAO<Admin> getAdminDAO() {
+		// TODO Auto-generated method stub
+		return new AdminDAO(conn);
+	}
+
+}
